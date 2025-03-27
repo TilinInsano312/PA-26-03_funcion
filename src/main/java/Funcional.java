91954cebf72016c5d28154094df7749f6500dcb3
Vicente Salazar <v.salazar11@ufromail.cl>
2025-03-27 12:12:26 -0300

    hotfix en funcional
    Implementacion de metodo para ingresar cantidad de notas y tambien la generacion aleatoria de numeros

--- src/main/java/Funcional.java
+++ src/main/java/Funcional.java
@@ -1,26 +1,44 @@
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Funcional {
-    List<Double> notas = Arrays.asList(1.2, 2.8, 3.7, 4.0, 5.0, 6.0);
+    Scanner scan = new Scanner(System.in);
 
     public Funcional() {
     }
+    public int cantidadNotas() {
+        int cantidad = 0;
+        System.out.print("Ingrese la cantidad de notas: ");
+        try{
+            cantidad = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("Ingrese un número válido");
+            scan.next();
+            cantidadNotas();
+        }
+        return cantidad;
+    }
 
-    public void contarNotas(){
+    public List<Double> generarNotas(int cantidad) {
+        return new Random().doubles(cantidad, 1 , 7) .boxed().toList();
+    }
+
+    public void contarNotas(List<Double> notas) {
+
         System.out.println("Notas: "+notas);
         int notasMayores =(int) notas.stream()
                 .filter( nota -> nota >= 4)
                 .count();
-        System.out.println("Notas mayores o iguales a 4: " + notasMayores);
+        System.out.println("Cantidad de notas mayores o iguales a 4: " + notasMayores);
         int notasEntre =(int) notas.stream()
                 .filter( nota -> nota >= 3.6&& nota < 4)
                 .count();
-        System.out.println("Notas entre 3.6 y 4: " + notasEntre);
+        System.out.println("Cantidad de notas entre 3.6 y 4: " + notasEntre);
         int notasMenores =(int) notas.stream()
                 .filter( nota -> nota < 3.6)
                 .count();
-        System.out.println("Notas menores a 3.6: " + notasMenores);
+        System.out.println("Cantidad de notas menores a 3.6: " + notasMenores);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Estructural e = new Estructural();
         Funcional f = new Funcional();
-        e.compararNotas(e.notas);
-        f.contarNotas();
+        e.compararNotas(e.notasRandom());
+        f.contarNotas(f.generarNotas(f.cantidadNotas()));
     }
 }
